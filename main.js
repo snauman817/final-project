7ad158e6d113cbebc51ad94d050067e3e333e751
closetmouse2 <thebestswordsman@gmail.com>
2018-12-04 15:50:06 -0500

    Re-worked code style to closer match co-workers. Prepared sample code to be worked into final project.

--- main.js
+++ main.js
@@ -1,41 +1,75 @@
-let score = 0;
-
 //below is all dynamic interactive functions
-window.onload=function(){
-  //selects target from html
-let targetBasic = document.querySelector(".target_basic");
-let targetHard = document.querySelector(".target_hard");
-let targetRare = document.querySelector(".target_rare");
+//selects target from html
 
 const basicShot = () => {
-  //makes html item dissapear (button in this case)
   targetBasic.style.display = "none";
-  //adds points to whatever counter is established
   score = score + 1;
-  //insert animation and audio functions here
   return score;
 };
 
 const hardShot = () => {
-  //makes html item dissapear (button in this case)
   targetHard.style.display = "none";
-  //adds points to whatever counter is established
   score = score + 3;
-  //insert animation and audio functions here
   return score;
 };
 
 const rareShot = () => {
-  //makes html item dissapear (button in this case)
   targetRare.style.display = "none";
-  //adds points to whatever counter is established
   score = score + 10;
-  //insert animation and audio functions here
   return score;
 };
+
+const test = () => {
+  if (interval > 0){
+  setInterval(appear(), 250);
+  return interval -= 1;
+  }
+};
+
+const appear = (targetType) => {
+   targetType.style.display = "block";
+   targetType.style.top = tCoordinates[Math.floor(Math.random() * 5)];
+   targetType.style.left = lCoordinates[Math.floor(Math.random() * 5)];
+};
+
+
+let targetBasic = document.createElement('button');
+let targetHard = document.createElement('button');
+let targetRare = document.createElement('button');
+targetBasic.textContent = "one";
+targetHard.textContent = "two";
+targetRare.textContent = "three";
+document.body.appendChild(targetBasic);
+document.body.appendChild(targetHard);
+document.body.appendChild(targetRare);
 
 targetBasic.addEventListener("click", basicShot);
 targetHard.addEventListener("click", hardShot);
 targetRare.addEventListener("click", rareShot);
 
+targetBasic.style.display = "none";
+targetHard.style.display = "none";
+targetRare.style.display = "none";
+targetBasic.style.position = 'absolute';
+targetHard.style.position = 'absolute';
+targetRare.style.position = 'absolute';
+
+let score = 0;
+
+const types = [targetBasic, targetHard, targetRare];
+
+let enemyList = [appear(targetBasic),appear(targetBasic)];
+
+const tCoordinates = ["200px", "300px", "400px", "500px", "600px", "700px"];
+
+const lCoordinates = ["200px", "300px", "400px", "500px", "600px", "700px"];
+
+
+module.exports = {
+ test,
+ basicShot,
+ hardShot,
+ rareShot,
+ tCoordinates,
+ targetBasic,
 };
---
+++ main.test.js
@@ -0,0 +1,7 @@
+const {test, basicShot, hardShot, rareShot,targetBasic,tCoordinates} = require('./main');
+
+describe('shot functions', () => {
+  test('basic item disapears', () => {
+   expect(click, targetBasic.style.display).toBe("none")
+});
+});
