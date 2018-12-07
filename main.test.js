c368e5ee25030d444870bb7cc7c2d240caa01eb7
Spencer Nauman <snauman817@gmail.com>
2018-12-07 14:31:16 -0500

    Map tests finished

--- main.test.js
+++ main.test.js
@@ -27,6 +27,55 @@ describe('Target Tests', () => {
   });
 });
 
+describe('Map tests', () => {
+  //map is successfully added to DOM
+  test('Map is successfully added to DOM', () => {
+    let itemsLength = document.querySelectorAll('section').length;
+    let newMap = addMap("whatever.jpg", [], 10);
+
+    expect(document.querySelectorAll('section').length > itemsLength).toBe(true);
+  });
+
+  //map is successfully removed from window
+  test('Map successfully removed from DOM when endMap() is called', () => {
+    let itemsLength = document.querySelectorAll('section').length;
+    let newMap = addMap("whatever.jpg", [], 10);
+
+    expect(document.querySelectorAll('section').length > itemsLength).toBe(true);
+
+    newMap.endMap();
+    expect(document.querySelectorAll('section').length > itemsLength).toBe(false);
+  });
+
+  //map's high score updates when the game ends
+  test("Map's high score updates when the game ends", () => {
+    let newMap = addMap("whatever.jpg", [], 10);
+    expect(newMap.currentScore).toBe(0);
+    expect(newMap.highScore === newMap.currentScore).toBe(true);
+
+    newMap.updateScore(10);
+    expect(newMap.currentScore).toBe(10);
+
+    newMap.endMap();
+    expect(newMap.highScore).toBe(10);
+  });
+
+  //map's time goes down when rollOverTime is called
+  test("map's time goes down when rollOverTime() is called", () => {
+    let newMap = addMap("whatever.jpg", [
+      createTarget(1),
+      createTarget(2),
+      createTarget(1),
+      createTarget(1),
+    ], 2);
+    expect(newMap.time).toBe(2);
+
+    newMap.rollOverTime();
+
+    expect(newMap.time).toBe(1);
+  });
+});
+
 // describe('Timer Tests', () => {
 //   test('timer appends numbers to doc', () => {
 //     expect(document.querySelectorAll('p').length).toBe(0);
